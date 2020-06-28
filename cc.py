ef559c6b142a1313128d231cf77f3d43a286d064
Tom <zippedfire@free.fr>
2020-06-28 11:34:15 +0200

    Update cc.py

--- cc.py
+++ cc.py
@@ -30,16 +30,24 @@ inp = [
 
 wid = len(inp[0])
 
+# compute the adjacency matrix
+
+# for each distinct character in the grid, create an empty set
 chars = {c: set() for line in inp for c in line}
 
+# fill that set with all the supporting letters
 for x in range(wid):
     for i in range(len(inp) - 1):
         chars[inp[i][x]].add(inp[i + 1][x])
 
 ans = []
 
+# while there are letters to add
 while chars:
+    # find the first letter that can be added
+    # i.e. find the first letter whose supporting letters are all present
     for c, l in chars.items():
+        # l - {c} gives all the supporting letters excluding the letter itself, to prevent cycles
         if (l - {c}).issubset(ans):
             ans.append(c)
             del chars[c]
